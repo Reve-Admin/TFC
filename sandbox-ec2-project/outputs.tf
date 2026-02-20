7b1a440ec165344000f88daf16cf7c5d32c818d7
Reve-Cloud <muthu.a@reve.cloud>
2026-02-21 05:28:20 +0530

    Update outputs for sandbox EC2 instance

--- sandbox-ec2-project/outputs.tf
+++ sandbox-ec2-project/outputs.tf
@@ -1,14 +1,15 @@
 output "instance_id" {
   description = "The ID of the hardened EC2 instance"
-  value       = aws_instance.hardened_server.id
+  value       = aws_instance.sandbox_server.id
 }
 
 output "instance_private_ip" {
   description = "The private IP address of the hardened EC2 instance"
-  value       = aws_instance.hardened_server.private_ip
+  value       = aws_instance.sandbox_server.private_ip
 }
 
 output "kms_key_arn" {
   description = "The ARN of the KMS key used for EBS encryption"
   value       = aws_kms_key.ebs_key.arn
+
 }
